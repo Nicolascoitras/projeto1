c27eb3b2c1509a300d5ccd21c4777941463a1102
Nicolas <70273074+Nicolascoitras@users.noreply.github.com>
2023-12-01 22:19:03 -0300

    Update Home.jsx

--- vite-project/src/routes/Home.jsx
+++ vite-project/src/routes/Home.jsx
@@ -6,14 +6,7 @@ return(
 
 <div> <div className='page'>
       
-            <tbody >
-            <tr> Titulo
-                  <td>Oi</td>
-            </tr>
-            <tr> Titulo2
-                  <td>Conteudo 2</td>
-            </tr>
-            </tbody>
+          
       </div></div>
 
 </>
